618c3951965cbc09d61644d47a970a01e99bb1af
Arnau García <arnau.garcia.gallego@gmail.com>
2017-06-29 16:17:34 +0200

    Configured ngx-translate

--- arnaugarcia/src/app/app.component.ts
+++ arnaugarcia/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {TranslateService} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  param = {value: 'world'};
+
+  constructor(translate: TranslateService) {
+    // this language will be used as a fallback when a translation isn't found in the current language
+    translate.setDefaultLang('en');
+    translate.addLangs(["en","es","ca"]);
+    translate.use("en");
+  }
+
 }
--- arnaugarcia/src/app/app.module.ts
+++ arnaugarcia/src/app/app.module.ts
@@ -2,13 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
+
+import {Http, HttpModule} from "@angular/http";
+import {TranslateHttpLoader} from "@ngx-translate/http-loader";
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: Http) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [Http]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]

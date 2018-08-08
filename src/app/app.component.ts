1333d093955c5708b466813334228fc252c6d3db
Arnau Garcia <agarciag@opentrends.net>
2018-08-08 10:17:48 +0200

    Added i18n files and dynamic navbar languages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,31 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {TranslateService} from "@ngx-translate/core";
 import {Title} from "@angular/platform-browser";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
 
+    menuOpen: string = '';
 
-  public constructor(
-    public translate: TranslateService,
-    private titleService: Title
-  ) {
 
-    translate.addLangs(["en", "es", "ca"]);
-    let browserLang = translate.getBrowserLang();
-    translate.use(browserLang.match(/en|es|ca/) ? browserLang : 'en');
-  }
 
-  public changeLanguage(lang: string){
-      this.translate.use(lang);
-      this.titleService.setTitle(this.translate.instant("APP.TITLE"));
-  }
+    public constructor(
+        public translate: TranslateService,
+        private titleService: Title
+    ) {
+
+        translate.addLangs(["en", "es", "ca"]);
+        let browserLang = translate.getBrowserLang();
+        translate.use(browserLang.match(/en|es|ca/) ? browserLang : 'en');
+    }
+
+    public changeLanguage(lang: string) {
+        this.translate.use(lang);
+        this.titleService.setTitle(this.translate.instant("APP.TITLE"));
+    }
 
 }

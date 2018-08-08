75da14727594dc1ee4fcfd850e554139aea114e2
Arnau Garcia <agarciag@opentrends.net>
2018-08-08 11:35:26 +0200

    Added header component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,10 +2,12 @@ import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import {HomeComponent} from './home.component';
 import {HOME_ROUTE} from "./home.route";
 import {RouterModule} from "@angular/router";
+import {SharedModule} from "../shared/shared.module";
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-    imports: [RouterModule.forChild([HOME_ROUTE])],
-    declarations: [HomeComponent],
+    imports: [SharedModule, RouterModule.forChild([HOME_ROUTE])],
+    declarations: [HomeComponent, HeaderComponent],
     schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class HomeModule {}

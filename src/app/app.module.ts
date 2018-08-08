56d85afdbcee260818d28f2b0a75f945fc68a72f
Arnau Garcia <agarciag@opentrends.net>
2018-08-08 11:28:46 +0200

    Added SharedModule and added footer to the structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,20 @@
 import {NgModule} from '@angular/core';
-import {BrowserModule, Title} from '@angular/platform-browser';
-import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
-import {TranslateHttpLoader} from '@ngx-translate/http-loader';
-import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
 import {ActiveMenuDirective, ErrorComponent, FooterComponent, MainComponent, NavbarComponent} from './layouts';
 import {AppRoutingModule} from "./app-routing.module";
-import {CommonModule} from "@angular/common";
 import {HomeModule} from "./home/home.module";
+import {SharedModule} from "./shared/shared.module";
 
-// AoT requires an exported function for factories
-export function HttpLoaderFactory(httpClient: HttpClient) {
-    return new TranslateHttpLoader(httpClient);
-}
 
 @NgModule({
     declarations: [MainComponent, NavbarComponent, FooterComponent, ActiveMenuDirective, ErrorComponent],
     imports: [
-        CommonModule,
         BrowserModule,
+        SharedModule,
         AppRoutingModule,
         HttpClientModule,
-        TranslateModule.forRoot({
-            loader: {
-                provide: TranslateLoader,
-                useFactory: HttpLoaderFactory,
-                deps: [HttpClient]
-            }
-        }),
         HomeModule
-    ],
-    providers: [
-        Title
     ],
     bootstrap: [MainComponent]
 })

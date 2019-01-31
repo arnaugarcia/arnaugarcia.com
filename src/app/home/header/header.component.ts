fc66646019cc06109c21a780c394aba56c00f034
Arnau García <arnau.garcia.gallego@gmail.com>
2019-01-31 11:38:09 +0100

    Started with logic for typing in the header

--- src/app/home/header/header.component.ts
+++ src/app/home/header/header.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import Typed from "typed.js";
+import {TranslateService} from "@ngx-translate/core";
 
 @Component({
     selector: 'app-header',
@@ -8,19 +9,31 @@ import Typed from "typed.js";
 })
 export class HeaderComponent implements OnInit {
 
+    private translatedText: string = '';
+    private typed: Typed;
 
-    constructor() {}
+    constructor(private translateService: TranslateService) {
+    }
 
     ngOnInit() {
+
+        this.translateService.stream('INTRO.SUBTITLE').subscribe((translation: string | any) => {
+            this.translatedText = translation;
+            this.initType();
+        });
+
+    }
+
+    private initType(): void {
+        this.typed = null;
         let options = {
-            strings: ["Books.", "Cinema.", "Paintings.", "Music."],
-            typeSpeed: 100,
-            backSpeed: 100,
+            strings: this.translatedText.split('|'),
+            typeSpeed: 50,
+            backSpeed: 50,
             showCursor: false,
             loop: true
         };
-
-        let typed = new Typed(".typing-element", options);
+        this.typed = new Typed(".typing-element", options);
     }
 
 }

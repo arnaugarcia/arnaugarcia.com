80174e20f95cd14b94de055e8919020633d400ea
Arnau García <arnau.garcia.gallego@gmail.com>
2019-02-06 09:23:36 +0100

    Added some documentation for navbar component methods

--- src/app/layouts/navbar/navbar.component.ts
+++ src/app/layouts/navbar/navbar.component.ts
@@ -11,7 +11,7 @@ import {animate, state, style, transition, trigger} from "@angular/animations";
     animations: [
         trigger('openClose', [
             state('false', style({
-                height: '0px'
+                height: '0px',
             })),
             transition('* => *', [
                 animate(150)
@@ -29,23 +29,33 @@ export class NavbarComponent {
     public constructor(
         public translate: TranslateService,
         private titleService: Title,
-        @Inject(DOCUMENT) private document: Document
-    ) {
+        @Inject(DOCUMENT) private document: Document) {
+
         translate.addLangs(['en', 'es', 'ca']);
         let browserLang = translate.getBrowserLang();
         translate.use(browserLang.match(/en|es|ca/) ? browserLang : 'en');
     }
 
+    /**
+     * Changes the language of the app
+     * @param lang a valid language [CA, ES, EN]
+     */
     public changeLanguage(lang: string) {
         this.translate.use(lang);
         this.titleService.setTitle(this.translate.instant("APP.TITLE"));
     }
 
+    /**
+     * This method is used for getting the current scroll for the page and set the class [small-menu && header-shadow]
+     */
     @HostListener('window:scroll', []) // for window scroll events
     onScroll() {
         this.currentScroll = this.document.documentElement.scrollTop
     }
 
+    /**
+     * Toggles the menu on click the burger menu
+     */
     toggleMenu(): void {
         this.mobileMenuOpen = !this.mobileMenuOpen;
     }

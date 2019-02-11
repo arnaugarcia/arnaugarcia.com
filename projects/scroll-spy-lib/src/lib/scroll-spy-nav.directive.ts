7f7ac90ec45fab13613df76f37c7b496233e21d1
Arnau García <arnau.garcia.gallego@gmail.com>
2019-02-11 12:41:41 +0100

    Getting the links and adding the active class

--- projects/scroll-spy-lib/src/lib/scroll-spy-nav.directive.ts
+++ projects/scroll-spy-lib/src/lib/scroll-spy-nav.directive.ts
@@ -1,12 +1,35 @@
-import {Directive, ElementRef} from '@angular/core';
+import {Directive, ElementRef, Input, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
-  selector: '[scrollSpyNav]'
+    selector: '[scrollSpyNav]'
 })
-export class ScrollSpyNavDirective {
+export class ScrollSpyNavDirective implements OnInit {
 
-  constructor(private element: ElementRef) {
+    readonly DEFAULT_ACTIVE_CLASS = 'active';
 
-  }
+    @Input()
+    private activeClass = this.DEFAULT_ACTIVE_CLASS;
+    private links: NodeList;
+    private ulTag: NodeList;
+
+    constructor(private renderer: Renderer2, private element: ElementRef) {
+    }
+
+    ngOnInit(): void {
+        this.getLinks();
+    }
+
+    private getLinks() {
+        this.ulTag = this.element.nativeElement.childNodes;
+        if (!this.ulTag && !(this.ulTag[0].nodeName == 'UL' || this.ulTag[0].nodeName == 'OL')) {
+            Error("The selected tag hasn't a list on ini");
+        }
+        this.links = this.ulTag[0].childNodes;
+        this.links.forEach((link: Node) => {
+            if (link.childNodes[0]) {
+                this.renderer.addClass(link.childNodes[0], this.activeClass);
+            }
+        });
+    }
 
 }

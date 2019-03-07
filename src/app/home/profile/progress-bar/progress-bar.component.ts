24488f9e6de58d0476e458f954a97e5f4dce8618
Arnau García <arnau.garcia.gallego@gmail.com>
2019-03-07 08:28:23 +0100

    Added progress-bar animation to profile section

--- src/app/home/profile/progress-bar/progress-bar.component.ts
+++ src/app/home/profile/progress-bar/progress-bar.component.ts
@@ -1,9 +1,36 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, ElementRef, HostListener, Input, OnInit} from '@angular/core';
+import {animate, state, style, transition, trigger} from "@angular/animations";
+import {ElementUtil} from "../../../shared/utils/element-util";
 
 @Component({
     selector: 'app-progress-bar',
     templateUrl: './progress-bar.component.html',
-    styleUrls: ['./progress-bar.component.css']
+    styleUrls: ['./progress-bar.component.css'],
+    animations: [
+        trigger('hideShow', [
+            state('hide', style({
+                opacity: 0,
+            })),
+            state('show', style({
+                opacity: 1,
+            })),
+            transition('hide => show', [
+                animate('1s')
+            ])
+        ]),
+        trigger('emptyFull', [
+            state('hide', style({
+                width: '0%',
+            })),
+            state('show', style({
+                width: '{{value}}%', // use interpolation
+            }), {params: {value: 0}}), // default parameters values required
+
+            transition('hide => show', [
+                animate('700ms')
+            ])
+        ]),
+    ]
 })
 export class ProgressBarComponent implements OnInit {
 
@@ -16,10 +43,18 @@ export class ProgressBarComponent implements OnInit {
     @Input()
     private max: number;
 
-    constructor() {
+    private state: string = 'hide';
+
+    constructor(private _el: ElementRef) {
     }
 
     ngOnInit(): void {
         console.log(`Min: ${this.min} Max: ${this.max} Value: ${this.value}`);
+    }
+
+    @HostListener('window:scroll', ['$event'])
+    checkScroll() {
+        const inView = ElementUtil.isScrolledIntoView(this._el.nativeElement);
+        this.state = inView ? 'show' : 'hide';
     }
 }

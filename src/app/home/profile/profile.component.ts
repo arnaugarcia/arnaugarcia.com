f5494258d2a2859ceee437c08638690b15f16d56
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2019-07-14 16:59:39 +0200

    Displaying progress bars with profile skills

--- src/app/home/profile/profile.component.ts
+++ src/app/home/profile/profile.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var $: any;
+
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
@@ -10,6 +12,19 @@ export class ProfileComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $('.progress-bar').each(function() {
+      $(this).appear(function() {
+        var percent = $(this).attr('aria-valuenow');
+        $(this).animate({'width' : percent + '%'});
+        $(this).find('.pb-number-box').animate({'opacity' : 1}, 1000);
+        $(this).find('.pb-number').countTo({
+          from: 0,
+          to: percent,
+          speed: 900,
+          refreshInterval: 30
+        });
+      });
+    });
   }
 
 }

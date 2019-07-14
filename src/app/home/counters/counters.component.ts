e91a9110d15278ee50b6fe65d0e2fa656d06ec3b
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2019-07-14 18:04:32 +0200

    Added counter jQuery logic

--- src/app/home/counters/counters.component.ts
+++ src/app/home/counters/counters.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var $: any;
+
 @Component({
   selector: 'app-counters',
   templateUrl: './counters.component.html',
@@ -10,6 +12,22 @@ export class CountersComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $('.counter-timer').each(function() {
+      $(this).appear(function() {
+        var number = $(this).attr('data-to');
+        $(this).countTo({
+          from: 0,
+          to: number,
+          speed: 1500,
+          refreshInterval: 10,
+          formatter: function (number, options) {
+            number = number.toFixed(options.decimals);
+            number = number.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+            return number;
+          }
+        });
+      });
+    });
   }
 
 }

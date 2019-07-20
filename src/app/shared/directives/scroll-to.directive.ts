b370442d0fe1fa6994fed1d7abfd117d7a28b8b6
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2019-07-20 17:15:36 +0200

    Added logic for scroll to directive

--- src/app/shared/directives/scroll-to.directive.ts
+++ src/app/shared/directives/scroll-to.directive.ts
@@ -1,10 +1,26 @@
-import { Directive } from '@angular/core';
+import {Directive, ElementRef, HostListener, Input} from '@angular/core';
 
 @Directive({
-  selector: 'a[appScrollTo]'
+  selector: 'a[scrollTo]'
 })
 export class ScrollToDirective {
 
-  constructor() { }
+  @Input()
+  smooth = true;
+
+  @HostListener('click', ['$event']) onClick($event) {
+    $event.preventDefault();
+    const section = this.element.nativeElement.href.split('#')[1];
+
+    if (this.smooth) {
+      document.querySelector(`#${section}`).scrollIntoView({behavior: 'smooth'});
+    } else {
+      document.querySelector(`#${section}`).scrollIntoView();
+    }
+  }
+
+  constructor(private element: ElementRef) {
+
+  }
 
 }

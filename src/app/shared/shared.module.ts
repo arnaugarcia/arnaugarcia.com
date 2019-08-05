c6f1ae83e417c5988c80ec5832a3d1f2bd398f90
Arnau García <arnau.garcia.gallego@gmail.com>
2019-08-05 13:52:48 +0200

    Replaced double quotes in shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,11 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {HttpClient} from "@angular/common/http";
-import {TranslateHttpLoader} from "@ngx-translate/http-loader";
-import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
-import {Title} from "@angular/platform-browser";
+import {HttpClient} from '@angular/common/http';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {Title} from '@angular/platform-browser';
 import {ScrollSpyDirective} from './directives/scroll-spy.directive';
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { ScrollToDirective } from './directives/scroll-to.directive';
 
 // AoT requires an exported function for factories

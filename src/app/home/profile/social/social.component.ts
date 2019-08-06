c9e4b8aa098cb65e4160b2c9ddb88845aaf8f673
Arnau García <arnau.garcia.gallego@gmail.com>
2019-08-06 12:57:24 +0200

    added href to social icons

--- src/app/home/profile/social/social.component.ts
+++ src/app/home/profile/social/social.component.ts
@@ -1,24 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import {Component} from '@angular/core';
+import {faTwitter} from '@fortawesome/free-brands-svg-icons';
 import {faInstagram} from '@fortawesome/free-brands-svg-icons/faInstagram';
 import {faGithub} from '@fortawesome/free-brands-svg-icons/faGithub';
 import {faLinkedin} from '@fortawesome/free-brands-svg-icons/faLinkedin';
 
 @Component({
-  selector: 'app-social',
-  templateUrl: './social.component.html',
-  styleUrls: ['./social.component.css']
+    selector: 'app-social',
+    templateUrl: './social.component.html',
+    styleUrls: ['./social.component.css']
 })
-export class SocialComponent implements OnInit {
+export class SocialComponent {
 
-  public faTwitter = faTwitter;
-  public faInstagram = faInstagram;
-  public faGithub = faGithub;
-  public faLinkedIn = faLinkedin;
-
-  constructor() {}
-
-  ngOnInit() {
-  }
+    public faTwitter = faTwitter;
+    public faInstagram = faInstagram;
+    public faGithub = faGithub;
+    public faLinkedIn = faLinkedin;
 
 }

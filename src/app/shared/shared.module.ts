49fa95a07c2c4004293414524ceeac96df67e754
Arnau García <arnau.garcia.gallego@gmail.com>
2019-08-07 14:30:47 +0200

    Removed fort-awesome module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,14 +4,12 @@ import {Title} from '@angular/platform-browser';
 import {ScrollSpyDirective} from './directives/scroll-spy.directive';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {ScrollToDirective} from './directives/scroll-to.directive';
-import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import {FormsModule} from '@angular/forms';
 import {TranslateModule} from '@ngx-translate/core';
 
 @NgModule({
     imports: [
         BrowserAnimationsModule,
-        FontAwesomeModule,
         TranslateModule
     ],
     providers: [
@@ -19,7 +17,7 @@ import {TranslateModule} from '@ngx-translate/core';
         TranslateModule
     ],
     declarations: [ScrollSpyDirective, ScrollToDirective],
-    exports: [CommonModule, FormsModule, ScrollSpyDirective, ScrollToDirective, FontAwesomeModule, TranslateModule]
+    exports: [CommonModule, FormsModule, ScrollSpyDirective, ScrollToDirective, TranslateModule]
 })
 export class SharedModule {
 }

ed17ebc4a9286692c3609c967d4d57d4a17c2c98
Arnau García <arnau.garcia.gallego@gmail.com>
2019-08-07 14:16:30 +0200

    Fixed imports and exports of TranslateModule

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,37 +1,25 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {HttpClient} from '@angular/common/http';
-import {TranslateHttpLoader} from '@ngx-translate/http-loader';
-import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
 import {Title} from '@angular/platform-browser';
 import {ScrollSpyDirective} from './directives/scroll-spy.directive';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {ScrollToDirective} from './directives/scroll-to.directive';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import {FormsModule} from '@angular/forms';
-
-// AoT requires an exported function for factories
-export function HttpLoaderFactory(httpClient: HttpClient) {
-    return new TranslateHttpLoader(httpClient);
-}
+import {TranslateModule} from '@ngx-translate/core';
 
 @NgModule({
     imports: [
-        TranslateModule.forRoot({
-            loader: {
-                provide: TranslateLoader,
-                useFactory: HttpLoaderFactory,
-                deps: [HttpClient]
-            }
-        }),
         BrowserAnimationsModule,
-        FontAwesomeModule
+        FontAwesomeModule,
+        TranslateModule
     ],
     providers: [
-        Title
+        Title,
+        TranslateModule
     ],
     declarations: [ScrollSpyDirective, ScrollToDirective],
-    exports: [CommonModule, FormsModule, TranslateModule, ScrollSpyDirective, ScrollToDirective, FontAwesomeModule]
+    exports: [CommonModule, FormsModule, ScrollSpyDirective, ScrollToDirective, FontAwesomeModule, TranslateModule]
 })
 export class SharedModule {
 }

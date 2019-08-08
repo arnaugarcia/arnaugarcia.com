9e140fe555746606194b7f26e02c8c6113350075
Arnau García <arnau.garcia.gallego@gmail.com>
2019-08-08 08:45:09 +0200

    Configured firebase prod and dev environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCwpZ5tI_ZBbE5dre4UCnGScspkDfswB-g',
+    authDomain: 'arnau-garcia.firebaseapp.com',
+    databaseURL: 'https://arnau-garcia.firebaseio.com',
+    projectId: 'arnau-garcia',
+    storageBucket: 'arnau-garcia.appspot.com',
+    messagingSenderId: '274807017013',
+    appId: '1:274807017013:web:2ede185fc96341fe'
+  }
 };

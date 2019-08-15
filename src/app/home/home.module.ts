4fa69dccdf35176435bd9c52550d5ccda1c08386
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2019-08-15 19:44:44 +0200

    Imported services module

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,13 +6,15 @@ import {SharedModule} from '../shared/shared.module';
 import {HeaderComponent} from './header/header.component';
 import {ProfileModule} from './profile/profile.module';
 import {PortfolioModule} from './portfolio/portfolio.module';
+import {ServicesModule} from './services/services.module';
 
 @NgModule({
     imports: [
         SharedModule,
         RouterModule.forChild([HOME_ROUTE]),
         ProfileModule,
-        PortfolioModule
+        PortfolioModule,
+        ServicesModule
     ],
     declarations: [
         HomeComponent,

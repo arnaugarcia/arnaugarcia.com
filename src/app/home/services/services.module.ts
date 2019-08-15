32b08f760d733b76c7f4545d6c511e8f53af5ffc
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2019-08-15 20:12:03 +0200

    Imported shared module in services module

--- src/app/home/services/services.module.ts
+++ src/app/home/services/services.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { ServicesComponent } from './services.component';
+import {SharedModule} from '../../shared/shared.module';
 
 @NgModule({
   declarations: [ServicesComponent],
   imports: [
-    CommonModule
-  ]
+    SharedModule
+  ],
+  exports: [ServicesComponent]
 })
 export class ServicesModule { }

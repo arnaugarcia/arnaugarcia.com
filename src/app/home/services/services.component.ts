4f66f59e45a04789a761cd42861e60cafb88cdb8
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2019-08-16 15:20:59 +0200

    Added service data in component

--- src/app/home/services/services.component.ts
+++ src/app/home/services/services.component.ts
@@ -1,10 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-
-interface IService {
-    title: string;
-    text: string;
-    logo: string;
-}
+import {IService} from './service.model';
 
 @Component({
     selector: 'app-services',
@@ -19,7 +14,12 @@ export class ServicesComponent implements OnInit {
     }
 
     ngOnInit() {
-        this.services.push({title: 'SERVICE.SERVICES.SERVICE1.TITLE', text: 'SERVICE.SERVICES.SERVICE1.TEXT', logo: 'icon-telescope'});
+        this.services.push({title: 'SERVICE.SERVICES.SERVICE1.TITLE', text: 'SERVICE.SERVICES.SERVICE1.TEXT', logo: 'icon-global'});
+        this.services.push({title: 'SERVICE.SERVICES.SERVICE2.TITLE', text: 'SERVICE.SERVICES.SERVICE2.TEXT', logo: 'icon-mobile'});
+        this.services.push({title: 'SERVICE.SERVICES.SERVICE3.TITLE', text: 'SERVICE.SERVICES.SERVICE3.TEXT', logo: 'icon-tools'});
+        this.services.push({title: 'SERVICE.SERVICES.SERVICE4.TITLE', text: 'SERVICE.SERVICES.SERVICE4.TEXT', logo: 'icon-circle-compass'});
+        this.services.push({title: 'SERVICE.SERVICES.SERVICE5.TITLE', text: 'SERVICE.SERVICES.SERVICE5.TEXT', logo: 'icon-layers'});
+        this.services.push({title: 'SERVICE.SERVICES.SERVICE6.TITLE', text: 'SERVICE.SERVICES.SERVICE6.TEXT', logo: 'icon-key'});
     }
 
 }

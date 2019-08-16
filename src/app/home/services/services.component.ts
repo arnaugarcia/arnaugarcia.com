5cca15068d83bc52c94857cad14936072c7974a7
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2019-08-16 15:13:34 +0200

    Indented component file

--- src/app/home/services/services.component.ts
+++ src/app/home/services/services.component.ts
@@ -1,24 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 interface IService {
-  title: string;
-  text: string;
-  logo: string;
+    title: string;
+    text: string;
+    logo: string;
 }
 
 @Component({
-  selector: 'app-services',
-  templateUrl: './services.component.html',
-  styleUrls: ['./services.component.css']
+    selector: 'app-services',
+    templateUrl: './services.component.html',
+    styleUrls: ['./services.component.css']
 })
 export class ServicesComponent implements OnInit {
 
-  public services: IService[] = [];
+    public services: IService[] = [];
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-    this.services.push({title: 'SERVICE.SERVICES.SERVICE1.TITLE', text: 'SERVICE.SERVICES.SERVICE1.TEXT', logo: 'icon-telescope'});
-}
+    ngOnInit() {
+        this.services.push({title: 'SERVICE.SERVICES.SERVICE1.TITLE', text: 'SERVICE.SERVICES.SERVICE1.TEXT', logo: 'icon-telescope'});
+    }
 
 }

e345b990f6976f5498b17e39ed8c67344c954a5c
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2019-08-28 19:48:58 +0200

    Added translations for resume items

--- src/app/home/resume/resume.component.ts
+++ src/app/home/resume/resume.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-resume',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./resume.component.css']
 })
 export class ResumeComponent implements OnInit {
+
+  items: number[] = [0, 1, 2, 3];
 
   constructor() { }
 

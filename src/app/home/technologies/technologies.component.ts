8db3eb528069925d2a3c058cc20cf6517d0e51e6
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2019-08-28 20:53:05 +0200

    Added owl carousel configurations and array of images

--- src/app/home/technologies/technologies.component.ts
+++ src/app/home/technologies/technologies.component.ts
@@ -7,6 +7,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TechnologiesComponent implements OnInit {
 
+  carouselOptions = {
+    margin: 25,
+    nav: false,
+    dots: false,
+    autoplay: true,
+    rewind: true,
+    autoplayTimeout: 3000,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true
+      },
+      600: {
+        items: 1,
+        nav: true
+      },
+      1000: {
+        items: 2,
+        nav: true,
+        loop: false
+      },
+      1500: {
+        items: 3,
+        nav: true,
+        loop: false
+      }
+    }
+  };
+
+  images = [
+    {
+      image: '/assets/images/technologies/java.svg'
+    },
+    {
+      image: '/assets/images/technologies/java.svg'
+    },
+    {
+      image: '/assets/images/technologies/java.svg'
+    },
+    {
+      image: '/assets/images/technologies/java.svg'
+    },
+    {
+      image: '/assets/images/technologies/java.svg'
+    },
+    {
+      image: '/assets/images/technologies/java.svg'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
--- src/app/home/technologies/technologies.module.ts
+++ src/app/home/technologies/technologies.module.ts
@@ -1,11 +1,13 @@
 import {NgModule} from '@angular/core';
 import {TechnologiesComponent} from './technologies.component';
 import {SharedModule} from '../../shared/shared.module';
+import {OwlModule} from 'ngx-owl-carousel';
 
 @NgModule({
   declarations: [TechnologiesComponent],
   imports: [
-    SharedModule
+    SharedModule,
+    OwlModule
   ],
   exports: [TechnologiesComponent]
 })

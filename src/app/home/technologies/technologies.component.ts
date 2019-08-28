653eb30c2ff3b94402e87a2ae3692cbd7d2a7b67
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2019-08-28 22:59:57 +0200

    Indented technologies component

--- src/app/home/technologies/technologies.component.ts
+++ src/app/home/technologies/technologies.component.ts
@@ -1,23 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {CarouselConstants} from './carousel.constants';
 import {TechnologiesService} from './technologies.service';
 import {ITechnology} from './technology.model';
 
 @Component({
-  selector: 'app-technologies',
-  templateUrl: './technologies.component.html',
-  styleUrls: ['./technologies.component.css']
+    selector: 'app-technologies',
+    templateUrl: './technologies.component.html',
+    styleUrls: ['./technologies.component.css']
 })
 export class TechnologiesComponent implements OnInit {
 
-  carouselOptions = CarouselConstants.carouselConfig;
+    carouselOptions = CarouselConstants.carouselConfig;
 
     technologies: ITechnology[] = [];
 
-  constructor(private technologiesService: TechnologiesService) { }
+    constructor(private technologiesService: TechnologiesService) {
+    }
 
-  ngOnInit() {
-      this.technologies = this.technologiesService.query();
-  }
+    ngOnInit() {
+        this.technologies = this.technologiesService.query();
+    }
 
 }

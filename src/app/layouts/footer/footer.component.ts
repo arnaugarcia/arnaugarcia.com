8d0aade7ed37d9f8740a31aeb3436d9ff39c3972
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2019-08-30 16:40:01 +0200

    Getting social networks of social service

--- src/app/layouts/footer/footer.component.ts
+++ src/app/layouts/footer/footer.component.ts
@@ -1,18 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {SocialService} from '../../home/profile/social/social.service';
+import {ISocialNetwork} from '../../home/profile/social/social.model';
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+    selector: 'app-footer',
+    templateUrl: './footer.component.html',
+    styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
     year: any;
+    socialNetworks: ISocialNetwork[] = [];
 
-  constructor() {
-    this.year = new Date().getFullYear();
-  }
+    constructor(private socialService: SocialService) {
+        this.year = new Date().getFullYear();
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.socialNetworks = this.socialService.querySocialNetworks();
+    }
 
 }

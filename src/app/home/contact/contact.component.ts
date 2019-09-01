2cbfe84f00672c9807ed5c94e5e001bde71d4934
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2019-09-01 16:49:06 +0200

    Implemented email service in contact section

--- src/app/home/contact/contact.component.ts
+++ src/app/home/contact/contact.component.ts
@@ -1,18 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import {IMail, Mail} from './email.model';
+import {EmailService} from './email.service';
+import {HttpResponse} from '@angular/common/http';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent implements OnInit {
+export class ContactComponent {
 
-  constructor() { }
+  mail: IMail = new Mail();
 
-  ngOnInit() {
-  }
+  constructor(private emailService: EmailService) { }
 
   onSubmit() {
-    console.log('Submit');
+    this.emailService.sendMessage(this.mail).subscribe((response: HttpResponse<void>) => {
+      console.log('Sucesss!');
+    }, error => {
+      console.error('Error!');
+    });
   }
 }

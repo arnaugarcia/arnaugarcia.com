a650e0365d36e41133fe89dbf39605aa9c715f48
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2019-09-01 14:11:37 +0200

    Created projects module, component and added dummy data there

---
+++ src/app/home/projects/projects.module.ts
@@ -0,0 +1,13 @@
+import {NgModule} from '@angular/core';
+import {ProjectsComponent} from './projects.component';
+import {SharedModule} from '../../shared/shared.module';
+
+@NgModule({
+    declarations: [ProjectsComponent],
+    imports: [
+        SharedModule
+    ],
+    exports: [ProjectsComponent]
+})
+export class ProjectsModule {
+}

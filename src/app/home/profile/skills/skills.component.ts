3a3a077fc4ccd9f4aa5758492cd5659d6ed63cdf
Arnau García <arnau.garcia.gallego@gmail.com>
2019-09-03 09:51:51 +0200

    Using IntersectionObserver instead of AppearJS

--- src/app/home/profile/skills/skills.component.ts
+++ src/app/home/profile/skills/skills.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+declare var $: any;
 
 @Component({
-  selector: 'app-skills',
-  templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.css']
+    selector: 'app-skills',
+    templateUrl: './skills.component.html'
 })
 export class SkillsComponent implements OnInit {
 
-  constructor() { }
+    ngOnInit() {
+        const element = document.querySelector('.skills');
+        if (element) {
+            const observer = new IntersectionObserver(entries => {
+                entries.forEach((skills) => {
+                    if (this.elementAppeared(skills)) {
+                        const progressBars = document.querySelectorAll('.progress-bar');
+                        progressBars.forEach(value => {
+                            const percent = value.attributes.getNamedItem('aria-valuenow');
+                            $(value).animate({'width': percent.value + '%'});
+                        });
+                    }
+                });
+            }, {rootMargin: '0px 0px', threshold: 0.00});
+            observer.observe(element);
+        }
+    }
 
-  ngOnInit() {
-  }
-
+    private elementAppeared(element) {
+        return element.intersectionRatio > 0;
+    }
 }

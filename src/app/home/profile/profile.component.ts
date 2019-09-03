d80c4becb7e9fd9a4f61b85a606dc19d3cc10abf
Arnau García <arnau.garcia.gallego@gmail.com>
2019-09-03 09:53:13 +0200

    Removed old logic for progress bars

--- src/app/home/profile/profile.component.ts
+++ src/app/home/profile/profile.component.ts
@@ -1,24 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-
-declare var $: any;
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+    selector: 'app-profile',
+    templateUrl: './profile.component.html',
+    styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-    $('.progress-bar').each(function() {
-      $(this).appear(function() {
-        const percent = $(this).attr('aria-valuenow');
-        $(this).animate({'width' : percent + '%'});
-        $(this).find('.pb-number-box').animate({'opacity' : 1}, 1000);
-      });
-    });
-  }
+export class ProfileComponent {
 
 }

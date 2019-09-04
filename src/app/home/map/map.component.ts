634a3493e5a64c2324b8246caaf2c0178fa0ca56
Arnau García <arnau.garcia.gallego@gmail.com>
2019-09-04 11:24:01 +0200

    Replaced agm with google maps vanilla

--- src/app/home/map/map.component.ts
+++ src/app/home/map/map.component.ts
@@ -1,16 +1,33 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
-import {MapTypeStyle} from '@agm/core';
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 import {mapStyle} from './map.constants';
+
+declare var google: any;
 
 @Component({
     selector: 'app-map',
     changeDetection: ChangeDetectionStrategy.OnPush,
     templateUrl: './map.component.html'
 })
-export class MapComponent {
+export class MapComponent implements OnInit {
     latitude = 41.3559809;
     longitude = 1.97556;
     zoom = 13;
     iconUrl = 'assets/images/map-icon.png';
-    styles: MapTypeStyle[] = mapStyle;
+    styles = mapStyle;
+
+    ngOnInit(): void {
+        const map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: this.latitude, lng: this.longitude},
+            zoom: this.zoom,
+            styles: mapStyle,
+            disableDefaultUI: true,
+            gestureHandling: 'none'
+        });
+
+        const marker = new google.maps.Marker({
+            position: {lat: this.latitude, lng: this.longitude},
+            icon: this.iconUrl,
+            map: map
+        });
+    }
 }
--- src/app/home/map/map.module.ts
+++ src/app/home/map/map.module.ts
@@ -1,16 +1,11 @@
 import {NgModule} from '@angular/core';
 import {MapComponent} from './map.component';
 import {SharedModule} from '../../shared/shared.module';
-import {AgmCoreModule} from '@agm/core';
-import {environment} from '../../../environments/environment';
 
 @NgModule({
     declarations: [MapComponent],
     imports: [
-        SharedModule,
-        AgmCoreModule.forRoot({
-            apiKey: environment.googleMapsApiKey
-        })
+        SharedModule
     ],
     exports: [
         MapComponent

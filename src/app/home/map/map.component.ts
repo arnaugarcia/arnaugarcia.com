0544afbf842915990d367201d0040fb1bf80e017
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2019-09-15 14:36:01 +0200

    Added intersection observer for map

--- src/app/home/map/map.component.ts
+++ src/app/home/map/map.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MapTypeStyle} from '@agm/core';
 import {mapStyle} from './map.constants';
 
@@ -6,10 +6,15 @@ import {mapStyle} from './map.constants';
     selector: 'app-map',
     templateUrl: './map.component.html'
 })
-export class MapComponent {
+export class MapComponent implements OnInit {
     latitude = 41.3559809;
     longitude = 1.97556;
     zoom = 13;
     iconUrl = 'assets/images/map-icon.png';
     styles: MapTypeStyle[] = mapStyle;
+
+    ngOnInit(): void {
+        console.log('Map Init');
+    }
+
 }

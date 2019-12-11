189f91903f347a64efee4f86cf2d41955d593a79
Arnau García <arnau.garcia.gallego@gmail.com>
2019-12-11 14:10:56 +0100

    Added Sallefy title, description translations for projects section

--- src/app/home/projects/projects.service.ts
+++ src/app/home/projects/projects.service.ts
@@ -9,50 +9,23 @@ export class ProjectsService {
   query(): IProject[] {
     return [
       {
-        categories: ['PROJECTS.CATEGORIES.IOT', 'PROJECTS.CATEGORIES.CONNECTED-CAR'],
+        categories: ['PROJECTS.CATEGORIES.IOT', 'PROJECTS.CATEGORIES.CONNECTED-CAR', 'PROJECTS.CATEGORIES.JAVA'],
         title: 'PROJECTS.ITEMS.0.TITLE',
         description: 'PROJECTS.ITEMS.0.DESCRIPTION',
         image: 'assets/images/projects/iot_car.png',
         imageDescription: 'PROJECTS.ITEMS.0.IMAGE-DESCRIPTION',
-        link: 'http://blog.arnaugarcia.com/connected-car',
+        link: null,
         date: 'PROJECTS.ITEMS.0.DATE'
       },
         // Add Sallefy project
       {
-        categories: ['PROJECTS.CATEGORIES.IOT', 'PROJECTS.CATEGORIES.CONNECTED-CAR'],
-        title: 'PROJECTS.ITEMS.0.TITLE',
-        description: 'PROJECTS.ITEMS.0.DESCRIPTION',
+        categories: ['PROJECTS.CATEGORIES.BACKEND', 'PROJECTS.CATEGORIES.JAVA'],
+        title: 'PROJECTS.ITEMS.1.TITLE',
+        description: 'PROJECTS.ITEMS.1.DESCRIPTION',
         image: 'assets/images/projects/iot_car.png',
-        imageDescription: 'PROJECTS.ITEMS.0.IMAGE-DESCRIPTION',
-        link: 'http://blog.arnaugarcia.com/connected-car',
-        date: 'PROJECTS.ITEMS.0.DATE'
-      },
-      {
-        categories: ['PROJECTS.CATEGORIES.IOT', 'PROJECTS.CATEGORIES.CONNECTED-CAR'],
-        title: 'PROJECTS.ITEMS.0.TITLE',
-        description: 'PROJECTS.ITEMS.0.DESCRIPTION',
-        image: 'assets/images/projects/iot_car.png',
-        imageDescription: 'PROJECTS.ITEMS.0.IMAGE-DESCRIPTION',
-        link: 'http://blog.arnaugarcia.com/connected-car',
-        date: 'PROJECTS.ITEMS.0.DATE'
-      },
-      {
-        categories: ['PROJECTS.CATEGORIES.IOT', 'PROJECTS.CATEGORIES.CONNECTED-CAR'],
-        title: 'PROJECTS.ITEMS.0.TITLE',
-        description: 'PROJECTS.ITEMS.0.DESCRIPTION',
-        image: 'assets/images/projects/iot_car.png',
-        imageDescription: 'PROJECTS.ITEMS.0.IMAGE-DESCRIPTION',
-        link: 'http://blog.arnaugarcia.com/connected-car',
-        date: 'PROJECTS.ITEMS.0.DATE'
-      },
-      {
-        categories: ['PROJECTS.CATEGORIES.IOT', 'PROJECTS.CATEGORIES.CONNECTED-CAR'],
-        title: 'PROJECTS.ITEMS.0.TITLE',
-        description: 'PROJECTS.ITEMS.0.DESCRIPTION',
-        image: 'assets/images/projects/iot_car.png',
-        imageDescription: 'PROJECTS.ITEMS.0.IMAGE-DESCRIPTION',
-        link: 'http://blog.arnaugarcia.com/connected-car',
-        date: 'PROJECTS.ITEMS.0.DATE'
+        imageDescription: 'PROJECTS.ITEMS.1.IMAGE-DESCRIPTION',
+        link: null,
+        date: 'PROJECTS.ITEMS.1.DATE'
       }
     ];
   }

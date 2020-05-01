0b1510e39aaa47901d9d3e1a3cfcddecfc368a8d
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2020-05-01 13:28:49 +0200

    Added RecaptchaV3Module in contact module

--- src/app/home/contact/contact.module.ts
+++ src/app/home/contact/contact.module.ts
@@ -1,11 +1,16 @@
 import {NgModule} from '@angular/core';
 import {SharedModule} from '../../shared/shared.module';
 import {ContactComponent} from './contact.component';
+import {RECAPTCHA_V3_SITE_KEY, RecaptchaV3Module} from 'ng-recaptcha';
 
 @NgModule({
   declarations: [ContactComponent],
   imports: [
-    SharedModule
+    SharedModule,
+    RecaptchaV3Module
+  ],
+  providers: [
+    { provide: RECAPTCHA_V3_SITE_KEY, useValue: '6LfT9PAUAAAAAFMnVRQeqraleGk7kdjnY1S-SIxC' },
   ],
   exports: [ContactComponent]
 })

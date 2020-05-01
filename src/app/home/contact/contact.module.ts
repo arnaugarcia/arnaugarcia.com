6a05cff7c01ed20dcb5ba54c6a3b7b166d297b23
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2020-05-01 13:43:17 +0200

    Revert "Added RecaptchaV3Module in contact module"
    This reverts commit 0b1510e3

--- src/app/home/contact/contact.module.ts
+++ src/app/home/contact/contact.module.ts
@@ -1,16 +1,11 @@
 import {NgModule} from '@angular/core';
 import {SharedModule} from '../../shared/shared.module';
 import {ContactComponent} from './contact.component';
-import {RECAPTCHA_V3_SITE_KEY, RecaptchaV3Module} from 'ng-recaptcha';
 
 @NgModule({
   declarations: [ContactComponent],
   imports: [
-    SharedModule,
-    RecaptchaV3Module
-  ],
-  providers: [
-    { provide: RECAPTCHA_V3_SITE_KEY, useValue: '6LfT9PAUAAAAAFMnVRQeqraleGk7kdjnY1S-SIxC' },
+    SharedModule
   ],
   exports: [ContactComponent]
 })

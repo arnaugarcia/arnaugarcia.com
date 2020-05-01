2df722ec41948be3b0d07b2a6070e46941220965
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2020-05-01 13:44:18 +0200

    Revert "Revert "Added reCaptcha site key to environment""
    This reverts commit 97707d1d

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,6 +9,9 @@ export const environment = {
     apiUrl: 'https://services.arnaugarcia.com',
     companyName: 'arnaugarcia.com',
     email: 'arnau.garcia.gallego@gmail.com'
+  },
+  reCaptcha: {
+    siteKey: '6LfT9PAUAAAAAFMnVRQeqraleGk7kdjnY1S-SIxC'
   }
 };
 

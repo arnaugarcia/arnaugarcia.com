e1756e9b120d9f4551012e8305e442f4f205ae07
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2020-11-25 10:50:12 +0100

    Added pointer to portfolio filters

--- src/app/home/portfolio/portfolio-filters/portfolio-filters.component.ts
+++ src/app/home/portfolio/portfolio-filters/portfolio-filters.component.ts
@@ -3,40 +3,41 @@ import {ChangeDetectionStrategy, Component, ElementRef, Input, OnInit, Renderer2
 declare var $: any;
 
 @Component({
-  selector: 'app-portfolio-filters',
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  templateUrl: './portfolio-filters.component.html',
-  styleUrls: ['./portfolio-filters.component.css']
+    selector: 'app-portfolio-filters',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    templateUrl: './portfolio-filters.component.html',
+    styleUrls: ['./portfolio-filters.component.css']
 })
 export class PortfolioFiltersComponent implements OnInit {
 
-  @Input()
-  public filters: string[] = [];
+    @Input()
+    public filters: string[] = [];
 
-  @ViewChild('filtersRef', {static: true})
-  public filtersRef: ElementRef;
+    @ViewChild('filtersRef', {static: true})
+    public filtersRef: ElementRef;
 
-  constructor(private renderer: Renderer2) { }
+    constructor(private renderer: Renderer2) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit(): void {
+    }
 
-  onFilter($event, filter) {
-    Object.keys(this.filtersRef.nativeElement.children).forEach((key) => {
-      this.renderer.removeClass(this.filtersRef.nativeElement.children[key].childNodes[0], 'current');
-    });
-    this.renderer.addClass($event.target, 'current');
-    const worksgrid = $('.row-portfolio');
-    setTimeout(function () {
-      worksgrid.isotope({
-        filter: filter
-      });
-    }, 300);
-    $('.portfolio-item', worksgrid).css({
-      'will-change': '',
-      'transform': '',
-      'opacity': '',
-    });
-  }
+    onFilter($event, filter): void {
+        Object.keys(this.filtersRef.nativeElement.children).forEach((key) => {
+            this.renderer.removeClass(this.filtersRef.nativeElement.children[key].childNodes[0], 'current');
+        });
+        this.renderer.addClass($event.target, 'current');
+        const worksgrid = $('.row-portfolio');
+        setTimeout(()  => {
+            worksgrid.isotope({
+                filter
+            });
+        }, 300);
+        $('.portfolio-item', worksgrid).css({
+            'will-change': '',
+            transform: '',
+            opacity: '',
+        });
+    }
 
 }

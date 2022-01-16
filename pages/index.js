519e6f2ed2f08c9635f619efb5dc67349c4fe716
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2022-01-16 17:48:40 +0100

    Solved portfolio dynamic import

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,9 @@ import Profile from "../sections/profile/profile";
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import Header from "../sections/header/header";
+import dynamic from "next/dynamic";
 
+const Portfolio = dynamic(() => import('../sections/portfolio/portfolio'), {ssr: false})
 
 export default function Home() {
     library.add(fab);
@@ -14,6 +16,9 @@ export default function Home() {
             </section>
             <section className="module" id="profile">
                 <Profile/>
+            </section>
+            <section className="module module-gray" id="portfolio">
+                <Portfolio/>
             </section>
         </div>
     )

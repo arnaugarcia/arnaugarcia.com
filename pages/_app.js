690e92131d764e28bc392149e3c5ab86696e3c07
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2022-01-21 17:42:43 +0100

    Implemented footer

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import '../styles/globals.scss'
 import '../styles/et-line.css'
 import Head from "next/head";
 import Navbar from "../sections/navbar/navbar";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 
 function MyApp({Component, pageProps}) {
@@ -16,6 +17,24 @@ function MyApp({Component, pageProps}) {
             <Navbar/>
             <div className={"wrapper"}>
                 <Component {...pageProps} />
+                <footer className="footer">
+                    <div className="container">
+                        <div className="row align-items-center">
+                            <div className="col-md-6">
+                                <span className="copyright">© {new Date().getFullYear()} Arnau Garcia, All Rights Reserved.</span>
+                            </div>
+                            <div className="col-md-6">
+                                <ul className="social-icons">
+                                    <li>
+                                        <a target="_blank" rel="noreferrer">
+                                            <FontAwesomeIcon icon={['fab', 'github']}/>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
             </div>
         </>
     )

7637b3f623ded90b61d65ce638a2ae19b6ed1131
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2022-02-01 19:04:21 +0100

    Created backend endpoint for captcha check

---
+++ pages/api/captcha.js
@@ -0,0 +1,16 @@
+export default function handler(req, res) {
+
+    if (req.method !== 'POST') {
+        res.status(405).json({ error: 'Method not allowed. Must be a POST' })
+    }
+
+    fetch('https://www.google.com/recaptcha/api/siteverify', {
+        method: 'post',
+        mode: 'cors',
+        body: {
+            'secret': '6Lcg7yoeAAAAAHt6GQ4DOt0uVSJHbL68QCqDhiiQ'
+        }
+    }).then((response) => {
+        console.log(response);
+    })
+}

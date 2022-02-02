b634b7aa3d192add2e0ede7d2756865a6ef86672
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2022-02-02 16:44:33 +0100

    Parsed URL query parameters

--- pages/api/captcha.js
+++ pages/api/captcha.js
@@ -1,16 +1,31 @@
-export default function handler(req, res) {
+import url from "url";
 
-    if (req.method !== 'POST') {
-        res.status(405).json({error: `Method ${req.method} not allowed`})
+export default function handler(request, response) {
+
+    if (request.method !== 'POST') {
+        response.status(405).json({error: `Method ${request.method} not allowed`})
+        return;
     }
 
-    fetch('https://www.google.com/recaptcha/api/siteverify', {
+    const urlParametersFromRequest = getUrlParametersFromRequest(request);
+
+    if (!urlParametersFromRequest['token']) {
+        response.status(400).json({error: 'Missing token parameter'});
+        return;
+    }
+
+
+    /*fetch('https://www.google.com/recaptcha/api/siteverify', {
         method: 'post',
         mode: 'cors',
         body: {
-            'secret': '6Lcg7yoeAAAAAHt6GQ4DOt0uVSJHbL68QCqDhiiQ'
+            'secret': process.env.RECAPTCHA_SITE_KEY
         }
     }).then((response) => {
-        console.log(response);
-    })
+        //console.log(response);
+    })*/
+}
+
+function getUrlParametersFromRequest(request) {
+    return url.parse(request.url, true).query;
 }

5c10733c7eefb9423699dcd1f0af6fe7795a418b
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2022-02-04 16:50:20 +0100

    Added script for loading map API

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import '../styles/et-line.css'
 import Head from "next/head";
 import Navbar from "../sections/navbar/navbar";
 import Footer from "../layout/footer";
+import {NextScript} from "next/document";
 
 function MyApp({Component, pageProps}) {
     return (
@@ -12,7 +13,15 @@ function MyApp({Component, pageProps}) {
                 <meta name="description"
                       content="Hola! Soy Arnau García, un programador con ganas de alcanzar nuevos retos y aportar nuevas cosas al mundo."/>
                 <meta name="author" content="Arnau Garcia"/>
-                <script src={`https://www.google.com/recaptcha/api.js?render=6Lcg7yoeAAAAACWp-OvBb2361m93f3fil53rzArx`}/>
+                <NextScript
+                    src={`https://www.google.com/recaptcha/api.js?render=6Lcg7yoeAAAAACWp-OvBb2361m93f3fil53rzArx`}
+                    async
+                    defer
+                />
+                <NextScript
+                    src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&callback=initMap&libraries=&v=weekly`}
+                    async
+                />
             </Head>
             <Navbar/>
             <div className={"wrapper"}>

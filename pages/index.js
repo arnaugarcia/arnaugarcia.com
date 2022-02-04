6aa854e917255303203abac4da32efcd0376612f
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2022-02-04 23:33:12 +0100

    Created basic logic for intersecting sections in the viewport

--- pages/index.js
+++ pages/index.js
@@ -9,18 +9,34 @@ import Technologies from "../sections/technologies/technologies";
 import Projects from "../sections/projects/projects";
 import Contact from "../sections/contact/contact";
 import Map from "../sections/map/map";
+import {useEffect, useRef, useState} from "react";
 
 const Portfolio = dynamic(() => import('../sections/portfolio/portfolio'), {ssr: false})
 
 export default function Home() {
     library.add(fab);
 
+    const projects = useRef();
+    const profile = useRef();
+
+    const [currentSection, setCurrentSection] = useState('home');
+
+    useEffect(() => {
+        const observer = new IntersectionObserver((entries) => {
+            let entry = entries[0];
+            if (entry && entry.isIntersecting) {
+                setCurrentSection(entry.target.id)
+            }
+        });
+        [projects.current, profile.current].forEach((section) => observer.observe(section));
+    }, [])
+
     return (
         <div id={"main"}>
             <section className={"module-header full-height parallax bg-dark bg-dark-30 header-light"} id="home">
-                <Header/>
+                <Header currentSection={currentSection}/>
             </section>
-            <section className="module" id="profile">
+            <section className="module" id="profile" ref={profile}>
                 <Profile/>
             </section>
             <section className="module module-gray" id="portfolio">
@@ -32,7 +48,7 @@ export default function Home() {
             <section className="module module-gray" id="resume">
                 <Summary/>
             </section>
-            <section className="module module-dark" id="projects">
+            <section className="module module-dark" id="projects" ref={projects}>
                 <Technologies/>
             </section>
             <section className="module module-gray">

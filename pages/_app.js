9c8451844d356f129f311acc33cd0f6c2a1b7b1c
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2022-02-07 18:41:04 +0100

    Transformed to hook function

--- pages/_app.js
+++ pages/_app.js
@@ -3,11 +3,10 @@ import '../styles/et-line.css'
 import Head from "next/head";
 import Navbar from "../sections/navbar/navbar";
 import Footer from "../layout/footer";
-import {NextScript} from "next/document";
 import {useState} from "react";
 import { appWithTranslation } from 'next-i18next';
 
-function MyApp({Component, pageProps}) {
+const MyApp = ({Component, pageProps}) => {
 
     const [currentSection, setCurrentSection] = useState('home');
 
@@ -22,7 +21,7 @@ function MyApp({Component, pageProps}) {
                 <meta name="description"
                       content="Hola! Soy Arnau García, un programador con ganas de alcanzar nuevos retos y aportar nuevas cosas al mundo."/>
                 <meta name="author" content="Arnau Garcia"/>
-                <NextScript
+                <script
                     src={`https://www.google.com/recaptcha/api.js?render=6Lcg7yoeAAAAACWp-OvBb2361m93f3fil53rzArx`}
                     async
                     defer

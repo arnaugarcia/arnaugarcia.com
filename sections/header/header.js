42137182e5e318f844da5d26c3e9649ebda5471c
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2022-02-07 18:49:08 +0100

    Translated intro section

--- sections/header/header.js
+++ sections/header/header.js
@@ -2,15 +2,18 @@ import styles from './header.module.css'
 import Particles from "react-tsparticles";
 import Typed from "./typed";
 import {particlesConstants} from "./particles.constants";
+import {useTranslation} from "next-i18next";
 
 export default function Header() {
+    const {t} = useTranslation('common');
+
     return (
         <>
             <Particles className={styles.particles} options={particlesConstants} />
             <div className={styles.text}>
                 <div className="row">
                     <div className="col-md-12">
-                        <h1 className="h1 m-b-15">Arnau Garcia</h1>
+                        <h1 className="h1 m-b-15">{t('INTRO.TITLE')}</h1>
                         <h5>
                             <Typed/>
                         </h5>
--- sections/header/typed.js
+++ sections/header/typed.js
@@ -1,13 +1,16 @@
 import {useEffect, useRef} from "react";
 import TypedT from 'typed.js';
+import {useTranslation} from "next-i18next";
 
 export default function Typed() {
+
+    const {t, i18n} = useTranslation('common');
 
     const element = useRef(null);
 
     useEffect(() => {
         const typed = new TypedT(element.current, {
-            strings: ["A creative web Developer... ", "Micro-services developer" , "Spring developer", "Angular developer", "JHipster enthusiast", "Coffee lover"],
+            strings: t('INTRO.SUBTITLE').split('|'),
             typeSpeed: 50,
             backSpeed: 50,
             showCursor: true,
@@ -17,7 +20,7 @@ export default function Typed() {
         return () => {
             typed.destroy();
         };
-    }, []);
+    }, [i18n.language]);
 
     return (<span ref={element}/>);
 

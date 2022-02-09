8d69150aaee3fab3c72dffa73381b0ff7da40d22
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2022-02-09 18:26:59 +0100

    Extracted typed constants

--- sections/header/typed.js
+++ sections/header/typed.js
@@ -1,6 +1,7 @@
 import {useEffect, useRef} from "react";
 import TypedT from 'typed.js';
 import {useTranslation} from "next-i18next";
+import {typedConstants} from "./typed.constants";
 
 export default function Typed() {
 
@@ -11,10 +12,7 @@ export default function Typed() {
     useEffect(() => {
         const typed = new TypedT(element.current, {
             strings: t('INTRO.SUBTITLE').split('|'),
-            typeSpeed: 50,
-            backSpeed: 50,
-            showCursor: true,
-            loop: true
+            ...typedConstants
         });
 
         return () => {

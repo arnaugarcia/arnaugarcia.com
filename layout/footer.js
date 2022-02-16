428ace52cb01e0c131acd9e30db4b81101c13865
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2022-02-16 20:42:40 +0100

    Translated footer

--- layout/footer.js
+++ layout/footer.js
@@ -1,12 +1,15 @@
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {useTranslation} from "next-i18next";
 
 export default function Footer() {
+    const {i18n} = useTranslation('common');
+
     return (
         <footer className="footer">
             <div className="container">
                 <div className="row align-items-center">
                     <div className="col-md-6">
-                        <span className="copyright">© {new Date().getFullYear()} Arnau Garcia, All Rights Reserved.</span>
+                        <span className="copyright">{i18n.t('FOOTER.CONTENT', {year: new Date().getFullYear()})}</span>
                     </div>
                     <div className="col-md-6">
                         <ul className="social-icons">

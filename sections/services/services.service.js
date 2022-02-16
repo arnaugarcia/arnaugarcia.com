2cc8081fcc6c8f9b2b435008544336838fdcfd13
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2022-02-16 20:20:01 +0100

    Created services service and added data there

---
+++ sections/services/services.service.js
@@ -0,0 +1,36 @@
+export const ServicesService = {
+    services: () => {
+        return [
+            {
+                icon: 'icon-global',
+                text: 'SERVICE.SERVICES.SERVICE1.TEXT',
+                title: 'SERVICE.SERVICES.SERVICE1.TITLE'
+            },
+            {
+                icon: 'icon-mobile',
+                text: 'SERVICE.SERVICES.SERVICE2.TEXT',
+                title: 'SERVICE.SERVICES.SERVICE2.TITLE'
+            },
+            {
+                icon: 'icon-tools',
+                text: 'SERVICE.SERVICES.SERVICE3.TEXT',
+                title: 'SERVICE.SERVICES.SERVICE3.TITLE'
+            },
+            {
+                icon: 'icon-circle-compass',
+                text: 'SERVICE.SERVICES.SERVICE4.TEXT',
+                title: 'SERVICE.SERVICES.SERVICE4.TITLE'
+            },
+            {
+                icon: 'icon-layers',
+                text: 'SERVICE.SERVICES.SERVICE5.TEXT',
+                title: 'SERVICE.SERVICES.SERVICE5.TITLE'
+            },
+            {
+                icon: 'icon-key',
+                text: 'SERVICE.SERVICES.SERVICE6.TEXT',
+                title: 'SERVICE.SERVICES.SERVICE6.TITLE'
+            }
+        ]
+    }
+}

7bda584600c063e15a68dbeee0a6b8e8024fb23c
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2022-02-16 19:04:54 +0100

    Translated projects page and added missing images

--- sections/projects/projects.js
+++ sections/projects/projects.js
@@ -1,32 +1,36 @@
 import Project from "./project";
+import {useTranslation} from "next-i18next";
+import {useEffect, useState} from "react";
+import {ProjectsService} from "./projects.service";
 
 export default function Projects() {
 
+    const {t} = useTranslation('common');
+    const [projects, setProjects] = useState([]);
+
     const totalProjects = 3;
+
+    useEffect(() => {
+        setProjects(ProjectsService.projects());
+    }, []);
 
     return (<div className="container">
         <div className={`row blog-grid`} style={{marginTop: '-200px'}}>
-            <Project
-                title={'CONNECTED CAR | IOT'}
-                description={'Using Spring Flow, Web Flux and Angular I\'ve started a new project that have the aim to make almost any car a connected car using the OBD2 port. The project consist in a IOT platform developed using spring that generates an RESTAFUL API that can be used in any application like (Android, iOS, Web applications...).'}
-                tags={['IOT', 'CONNECTED CAR', 'JAVA']}
-                link={'#'}
-                image={'https://arnaugarcia.com/assets/images/projects/iot_car.png'}
-            />
-            <Project
-                title={'CONNECTED CAR | IOT'}
-                description={'Using Spring Flow, Web Flux and Angular I\'ve started a new project that have the aim to make almost any car a connected car using the OBD2 port. The project consist in a IOT platform developed using spring that generates an RESTAFUL API that can be used in any application like (Android, iOS, Web applications...).'}
-                tags={['IOT', 'CONNECTED CAR', 'JAVA']}
-                link={'#'}
-                image={'https://arnaugarcia.com/assets/images/projects/iot_car.png'}
-            />
+            {projects.map((project) =>
+                <Project
+                    title={t(project.title)}
+                    description={t(project.description)}
+                    tags={project.categories.map((category) => t(category))}
+                    link={project.link}
+                    image={project.image}
+                />)}
         </div>
         {(totalProjects > 3) ?
             <div className="row">
                 <div className="col-md-12">
                     <div className="text-center">
-                        <div className="spacer m-b-30"></div>
-                        <a className="btn btn-brand"><span>Show more projects</span></a>
+                        <div className="spacer m-b-30"/>
+                        <a className="btn btn-brand"><span>{t('PROJECTS.MORE-BUTTON')}</span></a>
                     </div>
                 </div>
             </div> : ''

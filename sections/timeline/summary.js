e6665c3794150303e6664ce0db5445bbafec836c
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2022-02-16 19:47:27 +0100

    Translated summary page

--- sections/timeline/summary.js
+++ sections/timeline/summary.js
@@ -1,34 +1,44 @@
 import Milestone from "./milestone";
+import {useEffect, useState} from "react";
+import {MilestoneService} from "./milestone.service";
+import {useTranslation} from "next-i18next";
 
 export default function Summary() {
-    return (
 
+    const {t} = useTranslation('common');
+
+    const [milestones, setMilestones] = useState([]);
+
+    useEffect(() => {
+        setMilestones(MilestoneService.milestones());
+    }, [])
+
+    return (
         <div className="container">
             <div className="row">
                 <div className="col-md-12">
                     <div className="m-title c-align">
-                        <h2>SUMMARY</h2>
-                        <h6>EDUCATION AND EXPERIENCE</h6>
+                        <h2>{t('RESUME.TITLE')}</h2>
+                        <h6>{t('RESUME.SUBTITLE')}</h6>
                     </div>
                 </div>
             </div>
             <div className="row">
                 <div className="col-md-12">
                     <ul className="timeline">
-                        <Milestone title={'ASSISTANT TEACHER AT LA SALLE'}
-                                   place={'LA SALLE BARCELONA'}
-                                   date={'2019 - NOW'}
-                                   image={'https://arnaugarcia.com/assets/images/resume/teacher.png'}
-                                   description={'Together with Alfredo Rueda we develop and present the backend used for developing the final project of the Android development subject.'}/>
-                        <Milestone title={'ASSISTANT TEACHER AT LA SALLE'}
-                                   place={'LA SALLE BARCELONA'}
-                                   date={'2019 - NOW'}
-                                   image={'https://arnaugarcia.com/assets/images/resume/teacher.png'}
-                                   description={'Together with Alfredo Rueda we develop and present the backend used for developing the final project of the Android development subject.'}/>
+                        {milestones.map((milestone, index) =>
+                            <Milestone
+                                key={index}
+                                title={t(milestone.title)}
+                                place={t(milestone.subtitle)}
+                                date={t(milestone.year)}
+                                image={milestone.image}
+                                description={t(milestone.description)}/>
+                        )}
                     </ul>
                     <div className="text-center">
-                        <a className="btn btn-lg btn-gray" href="assets/resume/CV_ArnauGarcia_rev3.pdf">
-                            <span>DOWNLOAD CV</span>
+                        <a className="btn btn-lg btn-gray" href={"assets/resume/CV_ArnauGarcia_rev3.pdf"} target={'_blank'}>
+                            <span>{t('RESUME.CV-BUTTON')}</span>
                         </a>
                     </div>
                 </div>

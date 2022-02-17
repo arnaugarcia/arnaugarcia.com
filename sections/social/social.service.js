46ed2ead5a60e53b8d129570ae3bdbc0a0c0828c
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2022-02-17 13:27:37 +0100

    Created footer service and added data there

---
+++ sections/social/social.service.js
@@ -0,0 +1,26 @@
+export const SocialService = {
+    socialNetworks: () => {
+        return [
+            {
+                icon: 'github',
+                label: 'SOCIAL.GITHUB.LABEL',
+                link: 'https://github.com/arnaugarcia'
+            },
+            {
+                icon: 'instagram',
+                label: 'SOCIAL.INSTAGRAM.LABEL',
+                link: 'https://instagram.com/arnaugarcia97'
+            },
+            {
+                icon: 'linkedin',
+                label: 'SOCIAL.LINKEDIN.LABEL',
+                link: 'https://www.linkedin.com/in/arnaugarciagallego/'
+            },
+            {
+                icon: 'twitter',
+                label: 'SOCIAL.TWITTER.LABEL',
+                link: 'https://twitter.com/arnaugarcia97'
+            }
+        ]
+    }
+}

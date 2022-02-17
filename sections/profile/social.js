c4e6c58c3e2f97054d90f55e6c3a8e9c9a4963ad
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2022-02-17 13:41:33 +0100

    Use SocialService instead of hard-coded data

--- sections/profile/social.js
+++ sections/profile/social.js
@@ -1,38 +1,19 @@
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import styles from './social.module.css'
+import {useEffect, useState} from "react";
+import {SocialService} from "../social/social.service";
 
 export default function Social() {
 
-    const social = [
-        {
-            'id': '1',
-            'icon': 'github',
-            'label': 'SOCIAL.GITHUB.LABEL',
-            'link': 'https://github.com/arnaugarcia'
-        },
-        {
-            'id': '2',
-            'icon': 'instagram',
-            'label': 'SOCIAL.INSTAGRAM.LABEL',
-            'link': 'https://instagram.com/arnaugarcia97'
-        },
-        {
-            'id': '3',
-            'icon': 'linkedin',
-            'label': 'SOCIAL.LINKEDIN.LABEL',
-            'link': 'https://www.linkedin.com/in/arnaugarciagallego/'
-        },
-        {
-            'id': '4',
-            'icon': 'twitter',
-            'label': 'SOCIAL.TWITTER.LABEL',
-            'link': 'https://twitter.com/arnaugarcia97'
-        }
-    ];
+    const [socialNetworks, setSocialNetworks] = useState([]);
+
+    useEffect(() => {
+        setSocialNetworks(SocialService.socialNetworks());
+    }, [])
 
     return (
         <ul className={styles.widgetSocial}>
-            {social.map(({id, icon, label, link}) => (
+            {socialNetworks.map(({id, icon, label, link}) => (
                 <li key={id}>
                     <a className={'btn btn-sm'}
                        target="_blank"

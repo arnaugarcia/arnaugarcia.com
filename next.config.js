2849dd88cd08434da9214caed79df44e3048ccf5
Arnau Garcia <arnau@evpassport.com>
2023-11-12 12:28:40 +0100

    Started implementing i18n-export

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
-const { i18n } = require('./next-i18next.config');
+const {i18n} = require("./next-i18next.config");
 
-module.exports = {
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
   i18n
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
 }
+
+module.exports = nextConfig
